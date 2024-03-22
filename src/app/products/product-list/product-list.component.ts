6ffcc84b314c676fd375d390babbb2bc8d3e88da
ismail debbarh <usamdh07@gmail.com>
2024-03-22 00:04:25 +0000

    Render products in the component using async pipe

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,50 +1,37 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { NgIf, NgFor, NgClass } from '@angular/common';
+import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { EMPTY, Subscription, catchError } from 'rxjs';
+import { EMPTY, Observable, catchError } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
+  imports: [NgIf, NgFor, NgClass, ProductDetailComponent, AsyncPipe],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent {
   pageTitle = 'Products';
   errorMessage = '';
-  sub!: Subscription;
 
   //product service
   productService = inject(ProductService);
 
   // Products
-  products: Product[] = [];
+  readonly products$: Observable<Product[]> =
+    this.productService.products$.pipe(
+      catchError((error: string) => {
+        this.errorMessage = error;
+        return EMPTY;
+      }),
+    );
 
   // Selected product id to highlight the entry
   selectedProductId: number | undefined = undefined;
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
-  }
-
-  //hooks
-  ngOnInit(): void {
-    this.sub = this.productService.products$
-      .pipe(
-        catchError((error: string) => {
-          this.errorMessage = error;
-          return EMPTY;
-        }),
-      )
-      .subscribe((products) => {
-        this.products = products;
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
   }
 }
